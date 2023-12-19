81d9252e5a4269f2ea77fc407930508469854b68
ChrisBrasil10 <115044920+ChrisBrasil10@users.noreply.github.com>
2023-12-18 23:52:06 -0500

    Add files via upload

---
+++ Homework/hw08/polynomial.cpp
@@ -0,0 +1,21 @@
+/*
+Chris Brasil
+hw08.cpp
+December 6th, 2023
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Polynomial{
+    struct Node{
+        Node(int data, Node* aNode) : data(data), next(aNode) {}
+        int data;
+        Node* next;
+    };
+public:
+
+private:
+
+};
---
+++ Homework/hw08/polynomial.h
@@ -0,0 +1,12 @@
+#include "polynomial.cpp"
+
+#ifndef OBJECTORIENTED_POLYNOMIAL_H
+#define OBJECTORIENTED_POLYNOMIAL_H
+
+class Polynomial;
+
+struct Polynomial::Node;
+
+
+
+#endif //OBJECTORIENTED_POLYNOMIAL_H
